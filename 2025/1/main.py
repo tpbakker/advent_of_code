8d746086dcab1fe1c8ac8e02348436f2bbe73cc1
Tom Bakker <158465552+tpbakker@users.noreply.github.com>
2025-12-03 11:44:44 +0100

    Update main.py

--- 2025/1/main.py
+++ 2025/1/main.py
@@ -1,10 +1,10 @@
 with open("example.txt","r") as file:
-    example_day_1 = file.read()
+    example = file.read()
 
 with open("input.txt","r") as file:
-    input_day_1 = file.read()
+    input = file.read()
 
-print(moves)
+moves = input_day_1
 dial = 50
 answer_a = 0
 answer_b = 0
@@ -27,3 +27,4 @@ for rotation in moves:
 
 print(f"Answer A is: {answer_a}")
 print(f"Answer B is: {answer_b}")
+
