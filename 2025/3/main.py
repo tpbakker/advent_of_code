0df461de5deb8825ebad647fd97b669f91548f2b
Tom Bakker <158465552+tpbakker@users.noreply.github.com>
2025-12-03 21:15:04 +0100

    Update main.py

--- 2025/3/main.py
+++ 2025/3/main.py
@@ -4,26 +4,26 @@ with open("example.txt","r") as file:
 with open("input.txt","r") as file:
     input3 = file.read()
 
-def get_largest_number(input_list, min_remaining):
-    descending = sorted(input_list, reverse=True)
-    remaining_nrs = []
-    largest_nr = 0
-    n = 0
+def get_largest_number(input_bank: list, min_remaining: int):
+    descending_bank = sorted(input_bank, reverse=True)
+    remaining = []
+    largest_number = 0
+    index = 0
     if min_remaining == 0:
-        largest_nr = descending[n]
-        remaining_nrs = input_list[input_list.index(largest_nr) + 1:]
+        largest_number = descending_bank[index]
+        remaining = input_bank[input_bank.index(largest_number) + 1:]
     else:
-        while len(remaining_nrs) < min_remaining:
-            largest_nr = descending[n]
-            remaining_nrs = input_list[input_list.index(largest_nr) + 1:]
-            n += 1
-    return {"largest_number": largest_nr, "remaining": remaining_nrs}
+        while len(remaining) < min_remaining:
+            largest_number = descending_bank[index]
+            remaining = input_bank[input_bank.index(largest_number) + 1:]
+            index += 1
+    return {"largest_number": largest_number, "remaining": remaining}
 
-def get_joltage(bank, length):
+def get_joltage(input_bank: list, length: int):
     joltage = ""
     for n in reversed(range(length)):
-        largest_nr = get_largest_number(bank, n)["largest_number"]
-        bank = get_largest_number(bank, n)["remaining"]
+        largest_nr = get_largest_number(input_bank, n)["largest_number"]
+        input_bank = get_largest_number(input_bank, n)["remaining"]
         joltage += str(largest_nr)
     return joltage
 
@@ -35,8 +35,6 @@ outputs_b = []
 
 for bank in banks_as_lists:
     outputs_a.append(int(get_joltage(bank, 2)))
-
-for bank in banks_as_lists:
     outputs_b.append(int(get_joltage(bank, 12)))
 
 print(f"Answer A is: {sum(outputs_a)}")
