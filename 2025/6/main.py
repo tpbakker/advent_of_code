caef21e4ee7b2efb867925b40ef375600d41ff87
Tom Bakker <158465552+tpbakker@users.noreply.github.com>
2025-12-09 16:42:23 +0100

    Update main.py

--- 2025/6/main.py
+++ 2025/6/main.py
@@ -6,20 +6,61 @@ with open("input.txt","r") as file:
 
 data = input6
 rows = data.split("\n")
-ints = [[int(string) for string in x.split()] for x in rows[:-1]]
-length = len(ints[0])
-modifiers = rows[-1].split()
-results_a = []
+numbers = []
+numbers_a = [[int(string) for string in x.split()] for x in data.split("\n")[:-1]]
+numbers_b = []
+modifiers_a = rows[-1].split()
+modifiers_b = []
+width = 0
 answer_a = 0
 answer_b = 0
 
-def calculate_problem(n):
-    global ints, modifiers
-    calculation = f"{ints[0][n]} {modifiers[n]} {ints[1][n]} {modifiers[n]} {ints[2][n]} {modifiers[n]} {ints[3][n]}"
-    return eval(calculation)
+def calculate_problem(index, a_or_b):
+    global modifiers_a, modifiers_b, numbers_a, numbers_b
+    if a_or_b == "a":
+        calculation = f"{numbers_a[0][index]}"
+        for x in range(1, len(numbers_a)):
+            calculation += f"{modifiers_a[index]} {numbers_a[x][index]}"
+        return eval(calculation)
+    elif a_or_b == "b":
+        calculation = f"{numbers_b[index][0]}"
+        for x in range(1, len(numbers_b[index])):
+            if numbers_b[index][x] != "":
+                calculation += f"{modifiers_b[index]} {numbers_b[index][x]}"
+        print(f"{calculation} = {eval(calculation)}")
+        return eval(calculation)
 
-for n in range(length):
-    results_a.append(calculate_problem(n))
+    else:
+        return None
 
-print(f"Answer A is: {sum(results_a)}")
+for row in rows:
+    if len(row) > width:
+        width = len(row)
+
+for n in reversed(range(width)):
+    number = ""
+    for m in range(len(rows)):
+        try:
+            if m == len(rows) - 1:
+                if rows[m][n] != " ":
+                    modifiers_b.append(rows[m][n])
+            else:
+                number += f"{rows[m][n]}"
+        except IndexError:
+            number += ""
+
+    if number.isspace():
+        numbers_b.append(numbers)
+        numbers = []
+    elif number:
+        numbers.append(number.strip())
+numbers_b.append(numbers)
+
+for n in range(len(numbers_a)):
+    answer_a += calculate_problem(n, "a")
+
+for n in range(len(numbers_b)):
+    answer_b += calculate_problem(n, "b")
+
+print(f"Answer A is: {answer_a}")
 print(f"Answer B is: {answer_b}")
